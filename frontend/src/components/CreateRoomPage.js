63887a15dc433bda83ba71c907fdd8d905ac61f5
Cyrheel_ <cyrilb38@hotmail.com>
2022-02-23 16:47:42 +0100

    added some input and style to createroompage

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -17,7 +17,34 @@ class CreateRoomPage extends Component {
           </a>
         </div>
         <div className="create-body">
-          <h1>This is the Create Room Page</h1>
+          <h1>Create a Room</h1>
+          <div className="guest-can-pause">
+            <h2>Guest Control of Payback State</h2>
+            <div className="form-can-pause">
+              <div className="choice-container">
+                <label htmlFor="play-pause">Play/Pause</label>
+                <input
+                  type="radio"
+                  id="play-pause"
+                  name="gcp"
+                  className="input-gcp"
+                ></input>
+              </div>
+              <div className="choice-container">
+                <label htmlFor="no-control">No Control</label>
+                <input
+                  type="radio"
+                  id="no-control"
+                  name="gcp"
+                  className="input-gcp"
+                ></input>
+              </div>
+            </div>
+          </div>
+          <div className="votes-to-skip">
+            <h2>Votes Required to Skip Song</h2>
+            <input type="number" id="vote-skip" min={1} placeholder={1}></input>
+          </div>
         </div>
       </div>
     );
